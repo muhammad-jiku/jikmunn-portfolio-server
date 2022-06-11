47d860d87c7cb6d48e9818447a687cf9fb1ba74a
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-11 12:18:04 +0600

    database connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -20,28 +20,28 @@ app.get('/', (req, res) => {
   res.send('Hello there!');
 });
 
-// const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASS}@cluster0.vteap.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
+const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASS}@cluster0.vteap.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-// const run = async () => {
-//   try {
-//     await client.connect();
-//     const projectsCollection = client.db('projects').collection('project');
+const run = async () => {
+  try {
+    await client.connect();
+    const projectsCollection = client.db('projects').collection('project');
 
-//     //  displaying projects
-//     app.get('/projects', async (req, res) => {
-//       const projects = await projectsCollection.find({}).toArray();
-//       res.send(projects);
-//     });
-//   } finally {
-//     // await client.close();
-//   }
-// };
-// run().catch(console.dir);
+    //  displaying projects
+    app.get('/projects', async (req, res) => {
+      const projects = await projectsCollection.find({}).toArray();
+      res.send(projects);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
